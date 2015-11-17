917928b95db2e005317455fdf4305ae129c26ffd
JHades <jhades.dev@gmail.com>
2015-11-17 21:27:05 +0100

    added more typings

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
 
 /// <reference path="../node_modules/angular2/angular2.d.ts" />
-
+/// <reference path="../node_modules/angular2/router.d.ts" />
+/// <reference path="../node_modules/angular2/http.d.ts" />
 
 import {Component, bootstrap} from 'angular2/angular2';
 
