0c4de5171be2648f80ef266dc9b2fd529f47bf84
JHades <jhades.dev@gmail.com>
2015-11-17 21:42:34 +0100

    remove legacy typings

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,3 @@
-
-/// <reference path="../node_modules/angular2/angular2.d.ts" />
-/// <reference path="../node_modules/angular2/router.d.ts" />
-/// <reference path="../node_modules/angular2/http.d.ts" />
-
 import {Component, bootstrap} from 'angular2/angular2';
 
 
