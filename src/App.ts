e7d7eef549dc1a2262e8858430fcfda8ba86b5d7
Your Name <you@example.com>
2015-12-28 18:46:07 +0000

    upgrade to beta-0

--- src/App.ts
+++ src/App.ts
@@ -1,19 +1,20 @@
-import {Component, bootstrap} from 'angular2/angular2';
-
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
 
 @Component({
     selector: 'app',
     template: `<div>
-                    <input placeholder="Type here"  #input>
+                    <input placeholder="Type Hello World!"  #input>
                     <button (click)="onOk()">Push OK</button>
-
+                    <div>{{message}}</div>
                 </div>`
 })
 export class App {
 
+    message = "";
 
     onOk() {
-        alert('Hello World !!');
+        this.message = "Hello World!";
     }
 
 }
