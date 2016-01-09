1630f01f35d84a1c0c8577c942ddf600630580d4
Your Name <you@example.com>
2016-01-09 23:57:38 +0100

    upgraded to beta-1

--- src/HelloWorld.ts
+++ src/HelloWorld.ts
@@ -4,12 +4,11 @@ import {bootstrap} from 'angular2/platform/browser';
 
 @Component({
     selector: 'hello-world',
-    template: `<input placeholder="Type Hello World!" (keyup)="hello(input.value)" #input>
-                {{message}}`
+    template: `<input placeholder="Type hello world !" (keyup)="hello(input.value)" #input>{{message}}`
 })
 export class HelloWorld {
 
-    message = "";
+    private message = "";
 
     hello(value) {
         this.message = value;
