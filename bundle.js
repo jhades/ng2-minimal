bff665c362a8f6ffe0bd664ff7c3c1ea575b4a66
JHades <jhades.dev@gmail.com>
2016-05-04 10:14:34 +0200

    ng2-minimal - using systemjs-builder

---
+++ bundle.js
@@ -0,0 +1,10 @@
+var SystemBuilder = require('systemjs-builder');
+var builder = new SystemBuilder();
+
+builder.loadConfig('./system.config.js')
+  .then(function(){
+	  return builder.buildStatic('app', 'dist/bundle.js', {minify: false});
+  })
+  .then(function(){
+	  console.log('bundle built successfully!');
+  });
